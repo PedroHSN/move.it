25db82bad33b36d521d43de8ebb1f9568a919fa4
Pedro <pedrodti2017@gmail.com>
2021-04-25 22:42:55 -0300

    função do botão completei

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/components/ChallengeBox.module.css';
 
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -32,6 +32,8 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={completeChallenge}
+
                         >
                             Completei
                         </button>

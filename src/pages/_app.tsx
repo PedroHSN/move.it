1e0233f123516b7c1c27f61bf86cbd88e709f188
Pedro <pedrodti2017@gmail.com>
2021-04-27 21:46:38 -0300

    ServerSideProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,9 @@
 import '../styles/global.css'
 
-import { ChallengesProvider, ChallengesContext } from '../contexts/ChallengesContext'
-import { CountdownProvider } from '../contexts/CountdownContext'
-
-
 function MyApp({ Component, pageProps }) {
 
   return (
-
-    <ChallengesProvider>
-      <CountdownProvider>
-        <Component {...pageProps} />
-      </CountdownProvider>
-    </ChallengesProvider>
-
+    <Component {...pageProps} />
   )
 }
 
